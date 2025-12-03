56bf5fd1156534b8332de7a84962f604fca93dea
Qaliss <163942600+Qaliss@users.noreply.github.com>
2025-12-02 20:25:45 -0600

    Add files via upload

--- transcription.py
+++ transcription.py
@@ -10,7 +10,6 @@ logging.info("🔵 Logging initialized")
 
 
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
-# GROQ_API_KEY = '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'
 ai_client = Groq(api_key=GROQ_API_KEY)
 
 app = FastAPI(redirect_slashes=False)
